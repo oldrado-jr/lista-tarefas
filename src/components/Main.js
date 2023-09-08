d3c07a102b88c87a40de4d0f7f7c58184367908c
Oldrado Junior <oldradojunior@gmail.com>
2023-09-07 23:53:33 -0300

    feat(tasks): add tasks in the list

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,31 @@ import './Main.css';
 class Main extends Component {
   state = {
     taskName: '',
-    taskList: ['Fazer café', 'Beber água', 'Estudar'],
+    taskList: [],
   };
 
   handleChange = (e) => {
     this.setState({
       taskName: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { taskList } = this.state;
+    let { taskName } = this.state;
+
+    taskName = taskName.trim();
+
+    if (taskList.includes(taskName)) {
+      return;
+    }
+
+    const newTasks = [...taskList];
+
+    this.setState({
+      taskList: [...newTasks, taskName],
     });
   };
 
@@ -24,7 +43,7 @@ class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#" className="form">
+        <form action="#" onSubmit={this.handleSubmit} className="form">
           <input type="text" onChange={this.handleChange} value={taskName} />
           <button type="submit">
             <FaPlus />
