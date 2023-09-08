bfdcb3a5289c14f97331885fa1f5383ce2069de5
Oldrado Junior <oldradojunior@gmail.com>
2023-09-07 23:49:36 -0300

    feat: add tasks list style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 class Main extends Component {
   state = {
-    task: '',
+    taskName: '',
+    taskList: ['Fazer café', 'Beber água', 'Estudar'],
   };
 
   handleChange = (e) => {
     this.setState({
-      task: e.target.value,
+      taskName: e.target.value,
     });
   };
 
   render() {
-    const { task } = this.state;
+    const { taskName, taskList } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
         <form action="#" className="form">
-          <input type="text" onChange={this.handleChange} value={task} />
+          <input type="text" onChange={this.handleChange} value={taskName} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tasks">
+          {taskList.map((task) => (
+            <li key={task}>
+              {task}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
